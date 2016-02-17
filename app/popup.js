154f2b83ad43337f0c6c4a1c64ac8dbe1f217c03
long <forallandiyou@gmail.com>
2016-02-17 20:35:42 +0800

    changed: small ui bugs
    1. The dropdown text should be "Make Knoteup my new tab"
    2. There should be a second row in that drop down "Get the Knotes new tab extension" --> link that to the store page for Knotes

--- app/popup.js
+++ app/popup.js
@@ -10,13 +10,13 @@ $(document).ready(function(){
 
   var DEFAULT_NEW_TAB = 'is-default-new-tab';
   chrome.storage.local.get(DEFAULT_NEW_TAB, function(result){
-    $('.js-new-tab-option').attr('checked', result[DEFAULT_NEW_TAB]);
+    $('.js-new-tab-option').attr('checked', !result[DEFAULT_NEW_TAB]);
   });
 
   $('.js-new-tab-option').change(function(event){
     var isDefaultNewTab = $(event.currentTarget).prop('checked');
     var option = {};
-    option[DEFAULT_NEW_TAB] = isDefaultNewTab;
+    option[DEFAULT_NEW_TAB] = !isDefaultNewTab;
     chrome.storage.local.set(option, function(){
       window.close();
     });
